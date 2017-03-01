7ee696ed8450cca255dc14879d368948d4005784
Earle Nietzel <earle@longsight.com>
2017-03-01 09:40:44 -0500

    SAK-31713 move library and webjars to top level projects (#3262)

--- library/src/webapp/editor/elfinder/sakai/js/i18n.js
+++
@@ -1,7 +0,0 @@
-(function($) {
-
-// Quick fix for labels
-// Change 'Kind' to 'Type'
-elFinder.prototype.i18.en.messages.kind = 'Type';
-
-})(jQuery);
--- library/src/webapp/editor/elfinder/sakai/js/sakai.js
+++
@@ -1,6 +0,0 @@
-(function($) {
-
-$.sakai = {};
-$.sakai.elfinder = {};
-
-})(jQuery);
