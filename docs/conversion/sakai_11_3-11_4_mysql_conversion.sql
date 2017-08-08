d3e173ee2133bbd0f6a0ce64337643cb5182b3c6
Matthew Jones <jonespm@gmail.com>
2017-08-08 08:18:26 -0400

    SAK-32754 - Change drop default in conversion script

--- docs/conversion/sakai_11_3-11_4_mysql_conversion.sql
+++ docs/conversion/sakai_11_3-11_4_mysql_conversion.sql
@@ -1,5 +1,5 @@
 --
--- SAK-31840 drop defaults as its now managed in the POJO
+-- SAK-31840 update defaults as its now managed in the POJO
 --
-alter table GB_GRADABLE_OBJECT_T alter column IS_EXTRA_CREDIT drop default;
-alter table GB_GRADABLE_OBJECT_T alter column HIDE_IN_ALL_GRADES_TABLE drop default;
+alter table GB_GRADABLE_OBJECT_T alter column IS_EXTRA_CREDIT bit(1) DEFAULT NULL;;
+alter table GB_GRADABLE_OBJECT_T alter column HIDE_IN_ALL_GRADES_TABLE bit(1) DEFAULT NULL;;
