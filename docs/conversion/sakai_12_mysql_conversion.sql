8fc3213e329c598a45fa1037d9c99416111a494e
Code Hugger (Matthew Jones) <matthew@longsight.com>
2017-08-08 07:44:08 -0500

    SAK-32754 - Change drop default in conversion script (#2)

--- docs/conversion/sakai_12_mysql_conversion.sql
+++ docs/conversion/sakai_12_mysql_conversion.sql
@@ -203,10 +203,10 @@ ALTER TABLE pasystem_banner_dismissed DROP COLUMN user_eid;
 ALTER TABLE lesson_builder_pages ADD owned bit default false not null;
 -- END LSNBLDR-633
 --
--- SAK-31840 drop defaults as its now managed in the POJO
+-- SAK-31840 update defaults as its now managed in the POJO
 --
-alter table GB_GRADABLE_OBJECT_T alter column IS_EXTRA_CREDIT drop default;
-alter table GB_GRADABLE_OBJECT_T alter column HIDE_IN_ALL_GRADES_TABLE drop default;
+alter table GB_GRADABLE_OBJECT_T alter column IS_EXTRA_CREDIT bit(1) DEFAULT NULL;
+alter table GB_GRADABLE_OBJECT_T alter column HIDE_IN_ALL_GRADES_TABLE bit(1) DEFAULT NULL;
 
 -- BEGIN SAK-31819 Remove the old ScheduledInvocationManager job as it's not present in Sakai 12.
 DELETE FROM QRTZ_SIMPLE_TRIGGERS WHERE TRIGGER_NAME='org.sakaiproject.component.app.scheduler.ScheduledInvocationManagerImpl.runner';
