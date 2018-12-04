30194b5a08a890e254ea7105a082d68a266d56a2
Shawn <shawn.foster@uwo.ca>
2018-12-04 11:32:32 -0500

    SAK-41017 Updated conversion scripts for 19 to fix existing Site Unavailable sites and Invalid URL sites

--- docs/conversion/sakai_19_mysql_conversion.sql
+++ docs/conversion/sakai_19_mysql_conversion.sql
@@ -15,3 +15,10 @@ ALTER TABLE MFR_TOPIC_T ADD RESTRICT_PERMS_FOR_GROUPS BIT(1) DEFAULT FALSE;
 CREATE INDEX contentreview_provider_id_idx on CONTENTREVIEW_ITEM (providerId, externalId);
 
 -- End SAK-39967
+
+-- SAK-41017
+
+UPDATE SAKAI_SITE_PAGE SET layout = '0' WHERE page_id = '!error-100';
+UPDATE SAKAI_SITE_PAGE SET layout = '0' WHERE page_id = '!urlError-100';
+
+-- End of SAK-41017
