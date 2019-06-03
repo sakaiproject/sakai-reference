84e8a788816a4348830e87cf6258dbcf78b09a70
Miguel Pellicer <mpellicer@entornosdeformacion.com>
2019-06-03 18:16:11 +0200

    Update sakai_11_4-11_5_mysql_conversion.sql
    Fixing a typo in a query

--- docs/conversion/sakai_11_4-11_5_mysql_conversion.sql
+++ docs/conversion/sakai_11_4-11_5_mysql_conversion.sql
@@ -2,7 +2,7 @@
 -- Update camel case events
 UPDATE SAKAI_EVENT SET EVENT = 'sam.assessment.submit' WHERE EVENT = 'sam.assessmentSubmitted';
 UPDATE SAKAI_EVENT SET EVENT = 'sam.assessment.graded.auto' WHERE EVENT = 'sam.assessmentAutoGraded';
-UPDATE SAKAI_EVENT SET EVENT = 'sam.assessment.submit.auto"' WHERE EVENT = 'sam.assessmentAutoSubmitted';
+UPDATE SAKAI_EVENT SET EVENT = 'sam.assessment.submit.auto' WHERE EVENT = 'sam.assessmentAutoSubmitted';
 UPDATE SAKAI_EVENT SET EVENT = 'sam.assessment.submit.timer.thrd' WHERE EVENT = 'sam.assessmentTimedSubmitted';
 UPDATE SAKAI_EVENT SET EVENT = 'sam.pubassessment.remove' WHERE EVENT = 'sam.pubAssessment.remove';
 
