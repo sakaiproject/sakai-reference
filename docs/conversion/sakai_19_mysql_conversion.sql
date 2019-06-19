ba1ebee6e93e5af318f65a34e669d61b6783604f
Sam Ottenhoff <ottenhoff@longsight.com>
2019-06-19 15:28:43 -0400

    SAK-40288 fix up Rubric permissions

--- docs/conversion/sakai_19_mysql_conversion.sql
+++ docs/conversion/sakai_19_mysql_conversion.sql
@@ -212,3 +212,51 @@ ALTER TABLE rbc_rubric_criterions ADD CONSTRAINT FK_soau1ppw2wakbx8hemaaanubi FO
 -- SAK-40687
 ALTER TABLE GB_GRADABLE_OBJECT_T ADD EXTERNAL_DATA LONGTEXT NULL;
 -- END SAK-40687
+
+-- --------------------------------------------------------------------------------------------------------------------------------------
+-- backfill new permission into existing realms
+-- --------------------------------------------------------------------------------------------------------------------------------------
+
+-- for each realm that has a role matching something in this table, we will add to that role the function from this table
+CREATE TABLE PERMISSIONS_SRC_TEMP (ROLE_NAME VARCHAR(99), FUNCTION_NAME VARCHAR(99));
+
+
+INSERT INTO PERMISSIONS_SRC_TEMP VALUES ('maintain','rubrics.associator');
+INSERT INTO PERMISSIONS_SRC_TEMP VALUES ('maintain','rubrics.editor');
+INSERT INTO PERMISSIONS_SRC_TEMP VALUES ('maintain','rubrics.evaluator');
+INSERT INTO PERMISSIONS_SRC_TEMP VALUES ('maintain','rubrics.evaluee');
+INSERT INTO PERMISSIONS_SRC_TEMP VALUES ('access','rubrics.evaluee');
+INSERT INTO PERMISSIONS_SRC_TEMP VALUES ('Instructor','rubrics.associator');
+INSERT INTO PERMISSIONS_SRC_TEMP VALUES ('Instructor','rubrics.editor');
+INSERT INTO PERMISSIONS_SRC_TEMP VALUES ('Instructor','rubrics.evaluator');
+INSERT INTO PERMISSIONS_SRC_TEMP VALUES ('Instructor','rubrics.evaluee');
+INSERT INTO PERMISSIONS_SRC_TEMP VALUES ('Teaching Assistant','rubrics.evaluator');
+INSERT INTO PERMISSIONS_SRC_TEMP VALUES ('Teaching Assistant','rubrics.evaluee');
+INSERT INTO PERMISSIONS_SRC_TEMP VALUES ('Student','rubrics.evaluee');
+
+-- lookup the role and function number
+CREATE TABLE PERMISSIONS_TEMP (ROLE_KEY INTEGER, FUNCTION_KEY INTEGER);
+INSERT INTO PERMISSIONS_TEMP (ROLE_KEY, FUNCTION_KEY)
+SELECT SRR.ROLE_KEY, SRF.FUNCTION_KEY
+FROM PERMISSIONS_SRC_TEMP TMPSRC
+JOIN SAKAI_REALM_ROLE SRR ON (TMPSRC.ROLE_NAME = SRR.ROLE_NAME)
+JOIN SAKAI_REALM_FUNCTION SRF ON (TMPSRC.FUNCTION_NAME = SRF.FUNCTION_NAME);
+
+-- insert the new function into the roles of any existing realm that has the role (don't convert the "!site.helper")
+INSERT INTO SAKAI_REALM_RL_FN (REALM_KEY, ROLE_KEY, FUNCTION_KEY)
+SELECT
+    SRRFD.REALM_KEY, SRRFD.ROLE_KEY, TMP.FUNCTION_KEY
+FROM
+    (SELECT DISTINCT SRRF.REALM_KEY, SRRF.ROLE_KEY FROM SAKAI_REALM_RL_FN SRRF) SRRFD
+    JOIN PERMISSIONS_TEMP TMP ON (SRRFD.ROLE_KEY = TMP.ROLE_KEY)
+    JOIN SAKAI_REALM SR ON (SRRFD.REALM_KEY = SR.REALM_KEY)
+    WHERE SR.REALM_ID != '!site.helper'
+    AND NOT EXISTS (
+        SELECT 1
+            FROM SAKAI_REALM_RL_FN SRRFI
+            WHERE SRRFI.REALM_KEY=SRRFD.REALM_KEY AND SRRFI.ROLE_KEY=SRRFD.ROLE_KEY AND SRRFI.FUNCTION_KEY=TMP.FUNCTION_KEY
+    );
+
+-- clean up the temp tables
+DROP TABLE PERMISSIONS_TEMP;
+DROP TABLE PERMISSIONS_SRC_TEMP;
