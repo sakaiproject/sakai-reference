8a1e41ab148c6f033bab1433095eb6ef12e59749
Bryan de Oliveira Brettas <boliveira@entornosdeformacion.com>
2020-06-26 09:54:02 +0200

    SAK-43826: Rubrics - Support weighted criterions

--- docs/conversion/sakai_21_mysql_conversion.sql
+++ docs/conversion/sakai_21_mysql_conversion.sql
@@ -1,2 +1,7 @@
 -- clear unchanged bundle properties
 DELETE SAKAI_MESSAGE_BUNDLE from SAKAI_MESSAGE_BUNDLE where PROP_VALUE is NULL;
+
+-- SAK-43826 : Rubrics: Support weighted criterions
+
+ALTER TABLE rbc_rubric ADD COLUMN WEIGHTED bit(1) NOT NULL DEFAULT 0;
+ALTER TABLE rbc_criterion ADD COLUMN WEIGHT DOUBLE NULL DEFAULT 0;
