ec58ea712a9539fa688f2bdd63aede3bdb7cd193
Earle Nietzel <earle@longsight.com>
2020-08-27 11:45:41 -0400

    SAK-30207 possible missing contstraint on CONTENTREVIEW_ITEM

--- docs/conversion/sakai_21_mysql_conversion.sql
+++ docs/conversion/sakai_21_mysql_conversion.sql
@@ -1,2 +1,6 @@
 -- clear unchanged bundle properties
 DELETE SAKAI_MESSAGE_BUNDLE from SAKAI_MESSAGE_BUNDLE where PROP_VALUE is NULL;
+
+-- this constraint may have been missed, it is ok if this line fails just comment it out
+ALTER TABLE CONTENTREVIEW_ITEM ADD CONSTRAINT UK_8dngr1v68kkv4u11c1nvrjj1l UNIQUE (PROVIDERID, CONTENTID);
+
