64cfb5951418cc8c043122b3576be01f3b757c15
Paul Lukasewych <plukasew@uwo.ca>
2020-11-06 07:49:42 -0500

    SAK-44305 Messages > Save Draft > Recipient not Saved

--- docs/conversion/sakai_22_mysql_conversion.sql
+++ docs/conversion/sakai_22_mysql_conversion.sql
@@ -2,3 +2,15 @@
 UPDATE SAKAI_SITE_TOOL SET TITLE = 'Discussions' WHERE REGISTRATION = 'sakai.forums' AND TITLE = 'Forums';
 UPDATE SAKAI_SITE_PAGE SET TITLE = 'Discussions' WHERE TITLE = 'Forums';
 -- End SAK-40427
+
+-- SAK-44305
+create table MFR_DRAFT_RECIPIENT_T
+(ID bigint not null auto_increment,
+ TYPE int not null,
+ RECIPIENT_ID varchar(255) not null,
+ DRAFT_MSG_ID bigint not null,
+ BCC bit not null,
+ primary key (ID));
+
+create index MFR_DRAFT_REC_MSG_ID_I on MFR_DRAFT_RECIPIENT_T(DRAFT_MSG_ID);
+-- End SAK-44305
