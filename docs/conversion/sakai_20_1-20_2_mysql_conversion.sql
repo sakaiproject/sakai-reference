867c4eea7b68948fc59f64f971e14e8a8c1dc70a
Charles Severance <csev@umich.edu>
2020-11-17 09:43:49 -0500

    SAK-44636 - Add pl_lessonselection column (#98)
    Move to Sakai 20 scripts vs 21

--- docs/conversion/sakai_20_1-20_2_mysql_conversion.sql
+++ docs/conversion/sakai_20_1-20_2_mysql_conversion.sql
@@ -2,3 +2,10 @@
 UPDATE poll_option SET deleted = 0 WHERE DELETED IS NULL;
 ALTER TABLE poll_option MODIFY COLUMN DELETED BIT NOT NULL DEFAULT 0;
 -- End SAK-44420
+
+
+-- SAK-44636 - Add LTI Lessons Placement checkbox - By default it is off for future tool installations
+ALTER TABLE lti_tools ADD pl_lessonsselection TINYINT DEFAULT 0;
+-- Existing records needed to be switched on
+UPDATE lti_tools SET pl_lessonsselection = 1;
+-- END SAK-44636
--- docs/conversion/sakai_20_1-20_2_oracle_conversion.sql
+++ docs/conversion/sakai_20_1-20_2_oracle_conversion.sql
@@ -2,3 +2,9 @@
 UPDATE POLL_OPTION SET DELETED = 0 WHERE DELETED IS NULL;
 ALTER TABLE POLL_OPTION MODIFY DELETED NUMBER(1,0) DEFAULT 0 NOT NULL;
 -- End SAK-44420
+
+-- SAK-44636 - Add LTI Lessons Placement checkbox - By default it is off for future tool installations
+ALTER TABLE lti_tools ADD pl_lessonsselection NUMBER(1) DEFAULT 0;
+-- Existing records needed to be switched on
+UPDATE lti_tools SET pl_lessonsselection = 1;
+-- END SAK-44636
