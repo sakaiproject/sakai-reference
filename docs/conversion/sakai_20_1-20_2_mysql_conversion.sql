89b5f8ed8dc043021456d024f603e0fda98ce78f
Earle Nietzel <earle@longsight.com>
2020-11-18 17:21:20 -0500

    SAK-43497 ASN_ASSIGNMENT_PROPERTIES.VALUE conversion (#96)

--- docs/conversion/sakai_20_1-20_2_mysql_conversion.sql
+++ docs/conversion/sakai_20_1-20_2_mysql_conversion.sql
@@ -3,6 +3,9 @@ UPDATE poll_option SET deleted = 0 WHERE DELETED IS NULL;
 ALTER TABLE poll_option MODIFY COLUMN DELETED BIT NOT NULL DEFAULT 0;
 -- End SAK-44420
 
+-- SAK-43497
+alter table ASN_ASSIGNMENT_PROPERTIES modify VALUE varchar(4000) null;
+-- END SAK-43407
 
 -- SAK-44636 - Add LTI Lessons Placement checkbox - By default it is off for future tool installations
 ALTER TABLE lti_tools ADD pl_lessonsselection TINYINT DEFAULT 0;
--- docs/conversion/sakai_20_1-20_2_oracle_conversion.sql
+++ docs/conversion/sakai_20_1-20_2_oracle_conversion.sql
@@ -3,6 +3,13 @@ UPDATE POLL_OPTION SET DELETED = 0 WHERE DELETED IS NULL;
 ALTER TABLE POLL_OPTION MODIFY DELETED NUMBER(1,0) DEFAULT 0 NOT NULL;
 -- End SAK-44420
 
+-- SAK-43497
+alter table asn_assignment_properties add temp_value VARCHAR2(4000) NULL;
+update asn_assignment_properties set temp_value=dbms_lob.substr(value,4000,1);
+alter table asn_assignment_properties drop column value;
+alter table asn_assignment_properties rename column temp_value to value;
+-- End SAK-43497
+
 -- SAK-44636 - Add LTI Lessons Placement checkbox - By default it is off for future tool installations
 ALTER TABLE lti_tools ADD pl_lessonsselection NUMBER(1) DEFAULT 0;
 -- Existing records needed to be switched on
