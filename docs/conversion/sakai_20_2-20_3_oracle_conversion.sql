85781cbcfd8bce71bdf32b1368b2cfa870adee25
Adrian Fish <adrian.r.fish@gmail.com>
2021-03-09 14:37:32 +0000

    SAK-45137 rbc_rating update script
    https://jira.sakaiproject.org/browse/SAK-45137

--- docs/conversion/sakai_20_2-20_3_oracle_conversion.sql
+++ docs/conversion/sakai_20_2-20_3_oracle_conversion.sql
@@ -3,6 +3,13 @@ ALTER TABLE rbc_evaluation ADD COLUMN status NUMBER(1,0) NOT NULL;
 UPDATE rbc_evaluation SET status = 2;
 -- END SAK-42371
 
+-- START SAK-45137
+UPDATE rbc_rating SET title = '' WHERE title IS NULL;
+ALTER TABLE rbc_rating MODIFY title VARCHAR2(255) NOT NULL;
+UPDATE rbc_rating SET points = 0 WHERE points IS NULL;
+ALTER TABLE rbc_rating MODIFY points NUMBER NOT NULL;
+-- END SAK-45137
+
 -- SAK-44810 - Add $Resource.id.history
 -- This needs to rename a column in the DB because of a bug that keeps "settings"
 -- from working as needed for this feature.
