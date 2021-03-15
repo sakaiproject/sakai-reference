bfbc01619a63ec852fbfc47d3d6fd1d28fd3fe49
Earle Nietzel <earle@longsight.com>
2021-03-15 11:54:54 -0400

    Oracle changes

--- docs/conversion/sakai_21_oracle_conversion.sql
+++ docs/conversion/sakai_21_oracle_conversion.sql
@@ -21,36 +21,36 @@ UPDATE lti_tools SET pl_lessonsselection = 1;
 -- END SAK-44637
 
 -- SAK-45174 Rubrics metadata datetime conversion
-ALTER TABLE rbc_criterion DROP created;
-ALTER TABLE rbc_criterion DROP modified;
-ALTER TABLE rbc_evaluation DROP created;
-ALTER TABLE rbc_evaluation DROP modified;
-ALTER TABLE rbc_rating DROP created;
-ALTER TABLE rbc_rating DROP modified;
-ALTER TABLE rbc_rubric DROP created;
-ALTER TABLE rbc_rubric DROP modified;
-ALTER TABLE rbc_tool_item_rbc_assoc DROP created;
-ALTER TABLE rbc_tool_item_rbc_assoc DROP modified;
+ALTER TABLE rbc_criterion DROP COLUMN created;
+ALTER TABLE rbc_criterion DROP COLUMN modified;
+ALTER TABLE rbc_evaluation DROP COLUMN created;
+ALTER TABLE rbc_evaluation DROP COLUMN modified;
+ALTER TABLE rbc_rating DROP COLUMN created;
+ALTER TABLE rbc_rating DROP COLUMN modified;
+ALTER TABLE rbc_rubric DROP COLUMN created;
+ALTER TABLE rbc_rubric DROP COLUMN modified;
+ALTER TABLE rbc_tool_item_rbc_assoc DROP COLUMN created;
+ALTER TABLE rbc_tool_item_rbc_assoc DROP COLUMN modified;
 
-ALTER TABLE rbc_criterion ADD created DATETIME NULL;
-ALTER TABLE rbc_criterion ADD modified DATETIME NULL;
-ALTER TABLE rbc_evaluation ADD created DATETIME NULL;
-ALTER TABLE rbc_evaluation ADD modified DATETIME NULL;
-ALTER TABLE rbc_rating ADD created DATETIME NULL;
-ALTER TABLE rbc_rating ADD modified DATETIME NULL;
-ALTER TABLE rbc_rubric ADD created DATETIME NULL;
-ALTER TABLE rbc_rubric ADD modified DATETIME NULL;
-ALTER TABLE rbc_tool_item_rbc_assoc ADD created DATETIME NULL;
-ALTER TABLE rbc_tool_item_rbc_assoc ADD modified DATETIME NULL;
+ALTER TABLE rbc_criterion ADD created TIMESTAMP NULL;
+ALTER TABLE rbc_criterion ADD modified TIMESTAMP NULL;
+ALTER TABLE rbc_evaluation ADD created TIMESTAMP NULL;
+ALTER TABLE rbc_evaluation ADD modified TIMESTAMP NULL;
+ALTER TABLE rbc_rating ADD created TIMESTAMP NULL;
+ALTER TABLE rbc_rating ADD modified TIMESTAMP NULL;
+ALTER TABLE rbc_rubric ADD created TIMESTAMP NULL;
+ALTER TABLE rbc_rubric ADD modified TIMESTAMP NULL;
+ALTER TABLE rbc_tool_item_rbc_assoc ADD created TIMESTAMP NULL;
+ALTER TABLE rbc_tool_item_rbc_assoc ADD modified TIMESTAMP NULL;
 
-UPDATE rbc_criterion SET created = NOW() WHERE created IS NULL;
-UPDATE rbc_criterion SET modified = NOW() WHERE created IS NULL;
-UPDATE rbc_evaluation SET created = NOW() WHERE created IS NULL;
-UPDATE rbc_evaluation SET modified = NOW() WHERE created IS NULL;
-UPDATE rbc_rating SET created = NOW() WHERE created IS NULL;
-UPDATE rbc_rating SET modified = NOW() WHERE created IS NULL;
-UPDATE rbc_rubric SET created = NOW() WHERE created IS NULL;
-UPDATE rbc_rubric SET modified = NOW() WHERE created IS NULL;
-UPDATE rbc_tool_item_rbc_assoc SET created = NOW() WHERE created IS NULL;
-UPDATE rbc_tool_item_rbc_assoc SET modified = NOW() WHERE created IS NULL;
+UPDATE rbc_criterion SET created = current_timestamp WHERE created IS NULL;
+UPDATE rbc_criterion SET modified = current_timestamp WHERE created IS NULL;
+UPDATE rbc_evaluation SET created = current_timestamp WHERE created IS NULL;
+UPDATE rbc_evaluation SET modified = current_timestamp WHERE created IS NULL;
+UPDATE rbc_rating SET created = current_timestamp WHERE created IS NULL;
+UPDATE rbc_rating SET modified = current_timestamp WHERE created IS NULL;
+UPDATE rbc_rubric SET created = current_timestamp WHERE created IS NULL;
+UPDATE rbc_rubric SET modified = current_timestamp WHERE created IS NULL;
+UPDATE rbc_tool_item_rbc_assoc SET created = current_timestamp WHERE created IS NULL;
+UPDATE rbc_tool_item_rbc_assoc SET modified = current_timestamp WHERE created IS NULL;
 -- END SAK-45174
