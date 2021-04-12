ec795b1acfd985e96839f9bc3e0c28806525a892
Earle Nietzel <earle@longsight.com>
2021-04-12 14:22:17 -0400

    SAK-45137 Rubrics null values for criteria script

--- docs/conversion/sakai_21_0-21_1_oracle_conversion.sql
+++ docs/conversion/sakai_21_0-21_1_oracle_conversion.sql
@@ -2,3 +2,11 @@
 ALTER TABLE lti_tools ADD lti13_platform_public_old CLOB;
 ALTER TABLE lti_tools ADD lti13_platform_public_old_at DATE DEFAULT NULL;
 -- End SAK-45435
+
+-- START SAK-45137
+UPDATE rbc_rating SET title = 'Title' WHERE title IS NULL;
+ALTER TABLE rbc_rating MODIFY title VARCHAR2(255) NOT NULL;
+UPDATE rbc_rating SET points = 0 WHERE points IS NULL;
+ALTER TABLE rbc_rating MODIFY points FLOAT NOT NULL;
+-- END SAK-45137
++
