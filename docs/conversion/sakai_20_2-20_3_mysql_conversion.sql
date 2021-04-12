a3ba4a182974d537f1d8eea7b760918d010f7127
Earle Nietzel <earle@longsight.com>
2021-04-12 14:26:33 -0400

    SAK-45137 revert from 20.3 script

--- docs/conversion/sakai_20_2-20_3_mysql_conversion.sql
+++ docs/conversion/sakai_20_2-20_3_mysql_conversion.sql
@@ -3,13 +3,6 @@ ALTER TABLE rbc_evaluation ADD COLUMN status int(11) NOT NULL;
 UPDATE rbc_evaluation SET status = 2;
 -- END SAK-42371
 
--- START SAK-45137
-UPDATE rbc_rating SET title = '' WHERE title IS NULL;
-ALTER TABLE rbc_rating MODIFY title varchar(255) NOT NULL;
-UPDATE rbc_rating SET points = 0 WHERE points IS NULL;
-ALTER TABLE rbc_rating MODIFY points double NOT NULL;
--- END SAK-45137
-
 -- SAK-44810 - Add $Resource.id.history
 -- This needs to rename a column in the DB because of a bug that keeps "settings"
 -- from working as needed for this feature.
