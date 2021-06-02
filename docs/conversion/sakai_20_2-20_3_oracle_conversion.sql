b38bdf3bb8f5b6932698239ee9e25b8bc7db4c56
David P. Bauer <davidpbauer@users.noreply.github.com>
2021-06-02 12:02:10 -0400

    SAK-42371 Update Oracle conversion script for new status column in rbc_evaluation (#109)

--- docs/conversion/sakai_20_2-20_3_oracle_conversion.sql
+++ docs/conversion/sakai_20_2-20_3_oracle_conversion.sql
@@ -1,5 +1,5 @@
 -- START SAK-42371
-ALTER TABLE rbc_evaluation ADD COLUMN status NUMBER(1,0) NOT NULL;
+ALTER TABLE rbc_evaluation ADD status NUMBER(1,0) DEFAULT 1 NOT NULL;
 UPDATE rbc_evaluation SET status = 2;
 -- END SAK-42371
 
