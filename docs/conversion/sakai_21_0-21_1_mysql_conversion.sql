1a9adf85ebd0e8c3efa97b16acc1eb89eae5119f
Earle Nietzel <earle@longsight.com>
2021-06-04 09:54:50 -0400

    SAK-45137 Rubrics null values for criteria script (#116)

--- docs/conversion/sakai_21_0-21_1_mysql_conversion.sql
+++ docs/conversion/sakai_21_0-21_1_mysql_conversion.sql
@@ -2,3 +2,10 @@
 ALTER TABLE lti_tools ADD     lti13_platform_public_old MEDIUMTEXT NULL;
 ALTER TABLE lti_tools ADD     lti13_platform_public_old_at DATETIME NULL;
 -- End SAK-45435
+
+-- START SAK-45137
+UPDATE rbc_rating SET title = '' WHERE title IS NULL;
+ALTER TABLE rbc_rating MODIFY title varchar(255) NOT NULL;
+UPDATE rbc_rating SET points = 0 WHERE points IS NULL;
+ALTER TABLE rbc_rating MODIFY points double NOT NULL;
+-- END SAK-45137
