faf53ea5e7da83beb9b02f0692820d47c22494f6
Adrian Fish <adrian.r.fish@gmail.com>
2021-08-04 16:56:46 +0100

    SAK-43510 Added PRONOUNS to SAKAI_PERSON_T;

--- docs/conversion/sakai_21_1-21_2_mysql_conversion.sql
+++ docs/conversion/sakai_21_1-21_2_mysql_conversion.sql
@@ -3,3 +3,7 @@ ALTER TABLE lti_tools ADD     lti13_platform_public_old MEDIUMTEXT NULL;
 ALTER TABLE lti_tools ADD     lti13_platform_public_old_at DATETIME NULL;
 -- End SAK-45435
 
+-- SAK-43510
+ALTER TABLE SAKAI_PERSON_T ADD PRONOUNS varchar(255);
+-- End SAK-43510
+
--- docs/conversion/sakai_21_1-21_2_oracle_conversion.sql
+++ docs/conversion/sakai_21_1-21_2_oracle_conversion.sql
@@ -3,3 +3,7 @@ ALTER TABLE lti_tools ADD lti13_platform_public_old CLOB;
 ALTER TABLE lti_tools ADD lti13_platform_public_old_at DATE DEFAULT NULL;
 -- End SAK-45435
 
+-- SAK-43510
+ALTER TABLE SAKAI_PERSON_T ADD PRONOUNS VARCHAR(255);
+-- End SAK-43510
+
