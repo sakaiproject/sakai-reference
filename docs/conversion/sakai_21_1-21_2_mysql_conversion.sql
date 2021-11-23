46fdcd7cefffa19b9665a33ad4a4af9a5f8b7744
Charles Severance <csev@umich.edu>
2021-11-23 09:38:51 -0500

    SAK-46542 - Pull in because of merge conflicts (#142)

--- docs/conversion/sakai_21_1-21_2_mysql_conversion.sql
+++ docs/conversion/sakai_21_1-21_2_mysql_conversion.sql
@@ -7,3 +7,9 @@ ALTER TABLE lti_tools ADD     lti13_platform_public_old_at DATETIME NULL;
 ALTER TABLE SAKAI_PERSON_T ADD PRONOUNS varchar(255);
 -- End SAK-43510
 
+-- SAK-45491
+ALTER TABLE lti_tools ADD lti13_platform_public_next MEDIUMTEXT;
+ALTER TABLE lti_tools ADD lti13_platform_public_next_at DATETIME NULL;
+ALTER TABLE lti_tools ADD lti13_platform_private_next MEDIUMTEXT
+-- End SAK-45491
+
--- docs/conversion/sakai_21_1-21_2_oracle_conversion.sql
+++ docs/conversion/sakai_21_1-21_2_oracle_conversion.sql
@@ -7,3 +7,9 @@ ALTER TABLE lti_tools ADD lti13_platform_public_old_at DATE DEFAULT NULL;
 ALTER TABLE SAKAI_PERSON_T ADD PRONOUNS VARCHAR2(255);
 -- End SAK-43510
 
+-- SAK-45491
+ALTER TABLE lti_tools ADD lti13_platform_public_next CLOB;
+ALTER TABLE lti_tools ADD lti13_platform_public_next_at DATE NULL;
+ALTER TABLE lti_tools ADD lti13_platform_private_next CLOB
+-- End SAK-45491
+
