6c1ba12f2b68df8b1d1214cf6ec4843ced77ab90
Earle Nietzel <earle@longsight.com>
2021-12-07 17:10:47 -0500

    SAK-45692 FILE_CONVERSION_QUEUE ADD CONSTRAINT REFERENCE

--- docs/conversion/sakai_21_1-21_2_mysql_conversion.sql
+++ docs/conversion/sakai_21_1-21_2_mysql_conversion.sql
@@ -13,3 +13,7 @@ ALTER TABLE lti_tools ADD lti13_platform_public_next_at DATETIME NULL;
 ALTER TABLE lti_tools ADD lti13_platform_private_next MEDIUMTEXT
 -- End SAK-45491
 
+-- Start SAK-45692
+ALTER TABLE FILE_CONVERSION_QUEUE ADD CONSTRAINT UK_j0t1gd58buw5b0cwxayincy09 UNIQUE (`REFERENCE`);
+-- End SAK-45692
+
--- docs/conversion/sakai_21_1-21_2_oracle_conversion.sql
+++ docs/conversion/sakai_21_1-21_2_oracle_conversion.sql
@@ -13,3 +13,6 @@ ALTER TABLE lti_tools ADD lti13_platform_public_next_at DATE NULL;
 ALTER TABLE lti_tools ADD lti13_platform_private_next CLOB
 -- End SAK-45491
 
+-- Start SAK-45692
+ALTER TABLE FILE_CONVERSION_QUEUE ADD CONSTRAINT UK_j0t1gd58buw5b0cwxayincy09 UNIQUE (REFERENCE);
+-- End SAK-45692
