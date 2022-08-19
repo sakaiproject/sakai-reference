e3582c0a5164d455e769d227c25c953f44a9519e
Sam Ottenhoff <ottenhoff@longsight.com>
2022-08-19 09:25:08 -0400

    SAK-47582 add index on LTI tool_id to speed up Manage Tools on big instances

---
+++ docs/conversion/sakai_22_1-22_2_mysql_conversion.sql
@@ -0,0 +1,3 @@
+-- Begin SAK-47582
+ALTER TABLE lti_content ADD INDEX TOOL_IDX(tool_id);
+-- End SAK-47582
---
+++ docs/conversion/sakai_22_1-22_2_oracle_conversion.sql
@@ -0,0 +1,3 @@
+-- Begin SAK-47582
+create index TOOL_IDX on lti_content (TOOL_ID);
+-- End SAK-47582
