e07228a1ac14a10f3c1064152bf6beb784e3cd94
Jose Cebellan <josecebe@gmail.com>
2022-12-13 15:07:31 +0100

    SAK-48106 Capitalize table names

--- docs/conversion/sakai_24_oracle_conversion.sql
+++ docs/conversion/sakai_24_oracle_conversion.sql
@@ -1,4 +1,4 @@
 -- SAK-48106
-UPDATE user_audits_log audits INNER JOIN sakai_user_id_map idmap ON audits.user_id = idmap.eid SET audits.user_id = idmap.user_id;
-UPDATE user_audits_log audits INNER JOIN sakai_user_id_map idmap ON audits.action_user_id = idmap.eid SET audits.action_user_id = idmap.user_id;
+UPDATE user_audits_log audits INNER JOIN SAKAI_USER_ID_MAP idmap ON audits.user_id = idmap.eid SET audits.user_id = idmap.user_id;
+UPDATE user_audits_log audits INNER JOIN SAKAI_USER_ID_MAP idmap ON audits.action_user_id = idmap.eid SET audits.action_user_id = idmap.user_id;
 -- END SAK-48106
