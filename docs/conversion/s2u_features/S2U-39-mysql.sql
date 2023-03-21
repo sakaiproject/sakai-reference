a7b392ac74e8a25aa72d06fa21af2f857b21481d
Miguel Pellicer <mpellicer@entornosdeformacion.com>
2023-03-21 16:43:43 +0100

    S2U-39 rWiki conversion scripts

---
+++ docs/conversion/s2u_features/S2U-39-mysql.sql
@@ -0,0 +1,3 @@
+-- S2U-39 --
+ALTER TABLE rwikiobject ADD page_groups LONGTEXT DEFAULT '';
+-- END S2U-39 --
---
+++ docs/conversion/s2u_features/S2U-39-oracle.sql
@@ -0,0 +1,3 @@
+-- S2U-39 --
+ALTER TABLE rwikiobject ADD page_groups CLOB DEFAULT '';
+-- END S2U-39 --
