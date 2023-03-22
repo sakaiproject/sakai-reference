cea1cb29e255112d0083cd1a04813c041acede18
Miguel Pellicer <mpellicer@entornosdeformacion.com>
2023-03-22 11:51:35 +0100

    Update S2U-46-oracle.sql

--- docs/conversion/s2u_features/S2U-46-oracle.sql
+++ docs/conversion/s2u_features/S2U-46-oracle.sql
@@ -4,7 +4,7 @@ CREATE TABLE mc_site_synchronization (
   site_id varchar2(255) NOT NULL,
   team_id varchar2(255) NOT NULL,
   forced raw(1) DEFAULT NULL,
-  status number(10) DEFAULT NULL,
+  status number(1,0) DEFAULT NULL,
   status_updated_at timestamp(6) DEFAULT NULL,
   PRIMARY KEY (id),
   CONSTRAINT UKmc_ss UNIQUE (site_id,team_id)
@@ -15,7 +15,7 @@ CREATE TABLE mc_group_synchronization (
   parentId varchar2(99) DEFAULT NULL,
   group_id varchar2(255) NOT NULL,
   channel_id varchar2(255) NOT NULL,
-  status number(10) DEFAULT NULL,
+  status number(1,0) DEFAULT NULL,
   status_updated_at timestamp(6) DEFAULT NULL,
   PRIMARY KEY (id),
   CONSTRAINT UKmc_gs UNIQUE (parentId,group_id,channel_id),
@@ -29,13 +29,14 @@ CREATE TABLE mc_config_item (
 );
 
 CREATE TABLE mc_log (
-  id number(19) NOT NULL,
+  id number(19, 0) NOT NULL,
   context clob,
   event varchar2(255) DEFAULT NULL,
   event_date timestamp(6) DEFAULT NULL,
-  status number(10) DEFAULT NULL,
+  status number(1,0) DEFAULT NULL,
   PRIMARY KEY (id)
 );
 
 -- Generate ID using sequence and trigger
 CREATE SEQUENCE mc_log_seq START WITH 1 INCREMENT BY 1;
+-- S2U-46 --
