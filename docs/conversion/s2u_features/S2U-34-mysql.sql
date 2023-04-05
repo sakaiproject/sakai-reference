b0bc58c4fd04b7dde1b37efea40087c5c69aace4
bgarciaentornos <bgarcia@entornosdeformacion.com>
2023-04-05 13:47:07 +0200

    S2U-34 Modification for rubric association column (only mysql) (#197)

--- docs/conversion/s2u_features/S2U-34-mysql.sql
+++ docs/conversion/s2u_features/S2U-34-mysql.sql
@@ -3,4 +3,5 @@
 ALTER TABLE rbc_evaluation DROP INDEX `UKdn0jue890jn9p7vs6tvnsf2gf`;
 CREATE UNIQUE INDEX `UKqsk75a24pi108jpybtt16hshv` ON `rbc_evaluation` (evaluated_item_owner_id, evaluated_item_id, association_id) COMMENT '' ALGORITHM DEFAULT LOCK DEFAULT;
 UPDATE rbc_evaluation SET evaluated_item_owner_id = RIGHT(evaluated_item_owner_id, 36) where evaluated_item_owner_id like '/site/%';
+ALTER TABLE rbc_tool_item_rbc_assoc_conf MODIFY COLUMN parameters int;
 -- END S2U-34 --
