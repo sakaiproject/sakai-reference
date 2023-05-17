0a9efa4a723f915eaacf759614eb835abc2b90e2
bgarciaentornos <bgarcia@entornosdeformacion.com>
2023-05-17 12:12:46 +0200

    S2U-32 Assignments: add support for tags

---
+++ docs/conversion/s2u_features/S2U-33-oracle.sql
@@ -0,0 +1,9 @@
+-- S2U-33 --
+CREATE TABLE tagservice_tagassociation (
+  id varchar2(99) NOT NULL,
+  tag_id varchar2(255) NOT NULL,
+  item_id varchar2(255) NOT NULL,
+  PRIMARY KEY (id),
+  CONSTRAINT UK7tc7vcvcb0bw8moqdu3giik6o UNIQUE (tag_id,item_id)
+);
+-- END S2U-33 --
