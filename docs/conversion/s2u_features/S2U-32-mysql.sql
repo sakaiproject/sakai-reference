5aecb2c3d849045f215e7a23a0af5c90fddefe7e
bgarciaentornos <bgarcia@entornosdeformacion.com>
2023-05-17 12:43:38 +0200

    S2U-32 Assignments: add support for tags

---
+++ docs/conversion/s2u_features/S2U-32-mysql.sql
@@ -0,0 +1,9 @@
+-- S2U-32 --
+CREATE TABLE `tagservice_tagassociation` (
+  `id` varchar(99) NOT NULL,
+  `tag_id` varchar(255) NOT NULL,
+  `item_id` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK7tc7vcvcb0bw8moqdu3giik6o` (`tag_id`,`item_id`)
+);
+-- END S2U-32 --
