663731a5a82c59764d7cdb82c11031ea42feb9a5
Miguel Pellicer <mpellicer@entornosdeformacion.com>
2023-06-05 16:47:37 +0200

    Update S2U-32-S2U-28-mysql.sql

--- docs/conversion/s2u_features/S2U-32-S2U-28-mysql.sql
+++ docs/conversion/s2u_features/S2U-32-S2U-28-mysql.sql
@@ -3,7 +3,6 @@ CREATE TABLE `tagservice_tagassociation` (
   `id` varchar(99) NOT NULL,
   `tag_id` varchar(255) NOT NULL,
   `item_id` varchar(255) NOT NULL,
-  si tengo q meter columna para tool_id hacerlo aqui sobre el create aunque pertenezca a otra pieza..
   PRIMARY KEY (`id`),
   UNIQUE KEY `UK7tc7vcvcb0bw8moqdu3giik6o` (`tag_id`,`item_id`)
 );
