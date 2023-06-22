6b87a2e1dfbdfc719a504782d7f7bf0c8710fc34
Miguel Pellicer <mpellicer@entornosdeformacion.com>
2023-06-22 12:16:23 +0200

    Update S2U-39-oracle.sql

--- docs/conversion/s2u_features/S2U-39-oracle.sql
+++ docs/conversion/s2u_features/S2U-39-oracle.sql
@@ -1,6 +1,6 @@
 -- S2U-39 --
-Create table rwikipagegroups (
-    rwikiobjectid VARCHAR(36) NOT NULL,
-    groupid VARCHAR(99) NOT NULL
+CREATE TABLE rwikipagegroups (
+    rwikiobjectid VARCHAR2(36) NOT NULL,
+    groupid VARCHAR2(99) NOT NULL
 )
 -- END S2U-39 --
