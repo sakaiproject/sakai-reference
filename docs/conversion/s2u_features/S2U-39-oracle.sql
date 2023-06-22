bb2d2e0fd547c02b088249009eae714445571c5f
fadesta102 <64014315+fadesta102@users.noreply.github.com>
2023-06-22 08:58:48 +0200

    S2U-39 rWiki conversion scripts update

--- docs/conversion/s2u_features/S2U-39-oracle.sql
+++ docs/conversion/s2u_features/S2U-39-oracle.sql
@@ -1,3 +1,6 @@
 -- S2U-39 --
-ALTER TABLE rwikiobject ADD page_groups CLOB DEFAULT '';
+Create table rwikipagegroups (
+    rwikiobjectid VARCHAR(36) NOT NULL,
+    groupid VARCHAR(99) NOT NULL
+)
 -- END S2U-39 --
