e84e505534c4ee89b5819808df487ffd3a565e3f
stetsche <53173679+stetsche@users.noreply.github.com>
2023-10-10 09:14:50 +0200

    S2U-34 Rubrics conversion scripts (#222)

--- docs/conversion/sakai_24_mysql_conversion.sql
+++ docs/conversion/sakai_24_mysql_conversion.sql
@@ -48,3 +48,11 @@ CREATE INDEX IDX_CARDGAME_STAT_ITEM_PLAYER_ID ON CARDGAME_STAT_ITEM (ID, PLAYER_
 ALTER TABLE MFR_OPEN_FORUM_T ADD IS_FAQ_FORUM bit(1) DEFAULT b'0' NOT NULL;
 ALTER TABLE MFR_TOPIC_T ADD IS_FAQ_TOPIC bit(1) DEFAULT b'0' NOT NULL;
 -- END S2U-27 --
+
+-- S2U-34 --
+-- IMPORTANT: This index must be deleted and may have a different name, maybe UK_dn0jue890jn9p7vs6tvnsf2gf or similar
+ALTER TABLE rbc_evaluation DROP INDEX `UKdn0jue890jn9p7vs6tvnsf2gf`;
+CREATE UNIQUE INDEX `UKqsk75a24pi108jpybtt16hshv` ON `rbc_evaluation` (evaluated_item_owner_id, evaluated_item_id, association_id) COMMENT '' ALGORITHM DEFAULT LOCK DEFAULT;
+UPDATE rbc_evaluation SET evaluated_item_owner_id = RIGHT(evaluated_item_owner_id, 36) where evaluated_item_owner_id like '/site/%';
+ALTER TABLE rbc_tool_item_rbc_assoc_conf MODIFY COLUMN parameters int;
+-- END S2U-34 --
--- docs/conversion/sakai_24_oracle_conversion.sql
+++ docs/conversion/sakai_24_oracle_conversion.sql
@@ -26,3 +26,10 @@ CREATE INDEX IDX_CARDGAME_STAT_ITEM_PLAYER_ID ON CARDGAME_STAT_ITEM (ID, PLAYER_
 ALTER TABLE MFR_OPEN_FORUM_T ADD IS_FAQ_FORUM NUMBER(1,0) DEFAULT 0 NOT NULL;
 ALTER TABLE MFR_TOPIC_T ADD IS_FAQ_TOPIC NUMBER(1,0) DEFAULT 0 NOT NULL;
 -- END S2U-27 --
+
+-- S2U-34 --
+-- IMPORTANT: This index must be deleted and may have a different name, maybe UK_dn0jue890jn9p7vs6tvnsf2gf or similar
+DROP INDEX UKdn0jue890jn9p7vs6tvnsf2gf;
+CREATE UNIQUE INDEX UKqsk75a24pi108jpybtt16hshv ON RBC_EVALUATION (EVALUATED_ITEM_OWNER_ID, EVALUATED_ITEM_ID, ASSOCIATION_ID);
+UPDATE rbc_evaluation SET evaluated_item_owner_id = SUBSTR(evaluated_item_owner_id, -36) where evaluated_item_owner_id like '/site/%';
+-- END S2U-34 --
