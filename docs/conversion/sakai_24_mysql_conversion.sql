2400566823c4cac66f34603075dd7afc2847f17d
stetsche <53173679+stetsche@users.noreply.github.com>
2024-01-09 11:11:21 +0100

    S2U-32 S2U-28 Assignments and Messages: Tag support (#232)

--- docs/conversion/sakai_24_mysql_conversion.sql
+++ docs/conversion/sakai_24_mysql_conversion.sql
@@ -90,3 +90,54 @@ CREATE TABLE `SAM_ITEMHISTORICAL_T` (
 );
 -- END SAK-49591 --
 
+-- S2U-32 and S2U-28 --
+CREATE TABLE `tagservice_tagassociation` (
+  `id` varchar(99) NOT NULL,
+  `tag_id` varchar(255) NOT NULL,
+  `item_id` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK7tc7vcvcb0bw8moqdu3giik6o` (`tag_id`,`item_id`)
+);
+-- Permission added in 12 might not be present 
+MERGE INTO SAKAI_REALM_FUNCTION srf
+USING (
+SELECT -123 as function_key,
+'tagservice.manage' as function_name
+FROM dual
+) t on (srf.function_name = t.function_name)
+WHEN NOT MATCHED THEN
+INSERT (function_key, function_name)
+VALUES (SAKAI_REALM_FUNCTION_SEQ.NEXTVAL, t.function_name);
+-- Add this for every role able to create and manage tags on a site, you'll need to add the tool too
+INSERT INTO SAKAI_REALM_RL_FN VALUES((select REALM_KEY from SAKAI_REALM where REALM_ID = '!site.template'), (select ROLE_KEY from SAKAI_REALM_ROLE where ROLE_NAME = 'maintain'), (select FUNCTION_KEY from SAKAI_REALM_FUNCTION where FUNCTION_NAME = 'tagservice.manage'));
+INSERT INTO SAKAI_REALM_RL_FN VALUES((select REALM_KEY from SAKAI_REALM where REALM_ID = '!site.template.course'), (select ROLE_KEY from SAKAI_REALM_ROLE where ROLE_NAME = 'Instructor'), (select FUNCTION_KEY from SAKAI_REALM_FUNCTION where FUNCTION_NAME = 'tagservice.manage'));
+INSERT INTO SAKAI_REALM_RL_FN VALUES((select REALM_KEY from SAKAI_REALM where REALM_ID = '!site.template.course'), (select ROLE_KEY from SAKAI_REALM_ROLE where ROLE_NAME = 'Teaching Assistant'), (select FUNCTION_KEY from SAKAI_REALM_FUNCTION where FUNCTION_NAME = 'tagservice.manage'));
+-- Add this to populate existing sites with the permission
+CREATE TABLE PERMISSIONS_SRC_TEMP (ROLE_NAME VARCHAR(99), FUNCTION_NAME VARCHAR(99));
+INSERT INTO PERMISSIONS_SRC_TEMP values ('maintain','tagservice.manage');
+INSERT INTO PERMISSIONS_SRC_TEMP values ('Instructor','tagservice.manage');
+INSERT INTO PERMISSIONS_SRC_TEMP VALUES ('Teaching Assistant','tagservice.manage');
+
+CREATE TABLE PERMISSIONS_TEMP (ROLE_KEY INTEGER, FUNCTION_KEY INTEGER);
+INSERT INTO PERMISSIONS_TEMP (ROLE_KEY, FUNCTION_KEY)
+  SELECT SRR.ROLE_KEY, SRF.FUNCTION_KEY
+    from PERMISSIONS_SRC_TEMP TMPSRC
+    JOIN SAKAI_REALM_ROLE SRR ON (TMPSRC.ROLE_NAME = SRR.ROLE_NAME)
+    JOIN SAKAI_REALM_FUNCTION SRF ON (TMPSRC.FUNCTION_NAME = SRF.FUNCTION_NAME);
+
+INSERT INTO SAKAI_REALM_RL_FN (REALM_KEY, ROLE_KEY, FUNCTION_KEY)
+  SELECT SRRFD.REALM_KEY, SRRFD.ROLE_KEY, TMP.FUNCTION_KEY
+  FROM
+    (SELECT DISTINCT SRRF.REALM_KEY, SRRF.ROLE_KEY FROM SAKAI_REALM_RL_FN SRRF) SRRFD
+    JOIN PERMISSIONS_TEMP TMP ON (SRRFD.ROLE_KEY = TMP.ROLE_KEY)
+    JOIN SAKAI_REALM SR ON (SRRFD.REALM_KEY = SR.REALM_KEY)
+    WHERE SR.REALM_ID != '!site.helper' AND SR.REALM_ID NOT LIKE '!user.template%'
+    AND NOT EXISTS (
+        SELECT 1
+            FROM SAKAI_REALM_RL_FN SRRFI
+            WHERE SRRFI.REALM_KEY=SRRFD.REALM_KEY AND SRRFI.ROLE_KEY=SRRFD.ROLE_KEY AND SRRFI.FUNCTION_KEY=TMP.FUNCTION_KEY
+    );
+
+DROP TABLE PERMISSIONS_TEMP;
+DROP TABLE PERMISSIONS_SRC_TEMP;
+-- END S2U-32 and S2U-28 --
