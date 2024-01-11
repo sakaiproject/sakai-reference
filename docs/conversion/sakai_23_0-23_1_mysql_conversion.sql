68b171ba6471fc86a2ee412215e5238c3b355646
Charles Severance <csev@umich.edu>
2024-01-11 11:43:24 -0500

    Update sakai_23_0-23_1_mysql_conversion.sql

--- docs/conversion/sakai_23_0-23_1_mysql_conversion.sql
+++ docs/conversion/sakai_23_0-23_1_mysql_conversion.sql
@@ -10,3 +10,8 @@ UPDATE SAKAI_SITE_PAGE SET LAYOUT='0' WHERE PAGE_ID='!plussite-100';
 -- Fix any sites created with the template
 UPDATE SAKAI_SITE_PAGE SET LAYOUT='0' WHERE TITLE='Dashboard';
 -- SAK-49537 END
+
+--- SAK-49633
+--- Fix Site Type in !plussite template
+UPDATE SAKAI_SITE SET TYPE='course' WHERE SITE_ID = '!plussite';
+--- SAK-49633 END
