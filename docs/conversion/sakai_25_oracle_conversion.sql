e06f8afc7672bade9e2ab369dabf1893f3e5873c
Vicent Alfonso <vialrim2@gmail.com>
2024-06-04 13:17:52 +0200

    S2U-23 Samigo: show correction to student - conversion script addition

--- docs/conversion/sakai_25_oracle_conversion.sql
+++ docs/conversion/sakai_25_oracle_conversion.sql
@@ -49,6 +49,9 @@ CREATE TABLE rwikipagegroups (
 -- S2U-23 --
 ALTER TABLE SAM_PUBLISHEDFEEDBACK_T ADD SHOWCORRECTION NUMBER(1,0) DEFAULT 0 NOT NULL;
 ALTER TABLE SAM_ASSESSFEEDBACK_T ADD SHOWCORRECTION NUMBER(1,0) DEFAULT 0 NOT NULL;
+-- to preserve the complete functionality on assessments previous to the patch with 'showcorrectresponse' enabled
+UPDATE SAM_PUBLISHEDFEEDBACK_T SET showcorrection = 1 WHERE showcorrection = 0 AND showcorrectresponse = 1;
+UPDATE SAM_ASSESSFEEDBACK_T SET showcorrection = 1 WHERE showcorrection = 0 AND showcorrectresponse = 1;
 -- END S2U-23 --
 
 -- S2U-29 --
