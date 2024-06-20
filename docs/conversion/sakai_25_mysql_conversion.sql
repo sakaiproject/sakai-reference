88d730cf2eec57128709f8d101586886d826f57a
Charles Severance <csev@umich.edu>
2024-06-20 14:13:49 +0200

    Update sakai_25_mysql_conversion.sql SAK-46714

--- docs/conversion/sakai_25_mysql_conversion.sql
+++ docs/conversion/sakai_25_mysql_conversion.sql
@@ -301,3 +301,14 @@ CREATE TABLE `SAM_SECTIONGRADING_T` (
 ALTER TABLE SAM_ITEM_T ADD COLUMN ISFIXED BIT(1) DEFAULT FALSE NOT NULL;
 ALTER TABLE SAM_PUBLISHEDITEM_T ADD COLUMN ISFIXED BIT(1) DEFAULT FALSE NOT NULL;
 -- END S2U-19 --
+
+-- SAK-46714 --
+ALTER TABLE `lti_tools` DROP `lti13_platform_public_next`;
+ALTER TABLE `lti_tools` DROP `lti13_platform_public_next_at`;
+ALTER TABLE `lti_tools` DROP `lti13_platform_private_next`;
+ALTER TABLE `lti_tools` DROP `lti13_platform_public`;
+ALTER TABLE `lti_tools` DROP `lti13_platform_private`;
+ALTER TABLE `lti_tools` DROP `lti13_platform_public_old`;
+ALTER TABLE `lti_tools` DROP `lti13_platform_public_old_at`; 
+-- END SAK-46714 --
+
