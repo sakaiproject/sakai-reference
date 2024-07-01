df667227380065754f610b25e64b149340ba9f56
Vicent Alfonso <81161239+V100X@users.noreply.github.com>
2024-07-01 11:26:29 +0200

    S2U-23 Samigo: show correction to student - conversion script addition (#243)

--- docs/conversion/s2u_features/S2U-23-mysql.sql
+++ docs/conversion/s2u_features/S2U-23-mysql.sql
@@ -1,4 +1,7 @@
 -- S2U-23 --
 ALTER TABLE SAM_PUBLISHEDFEEDBACK_T ADD SHOWCORRECTION bit(1) DEFAULT b'0' NOT NULL;
 ALTER TABLE SAM_ASSESSFEEDBACK_T ADD SHOWCORRECTION bit(1) DEFAULT b'0' NOT NULL;
+-- to preserve the complete functionality on assessments previous to the patch with 'showcorrectresponse' enabled
+UPDATE SAM_PUBLISHEDFEEDBACK_T SET showcorrection = 1 WHERE showcorrection = 0 AND showcorrectresponse = 1;
+UPDATE SAM_ASSESSFEEDBACK_T SET showcorrection = 1 WHERE showcorrection = 0 AND showcorrectresponse = 1;
 -- END S2U-23 --
