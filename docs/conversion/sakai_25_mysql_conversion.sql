ed2621bee2c3f8db6e8024fc8efcafe2e865483f
Sam Ottenhoff <ottenhoff@longsight.com>
2026-06-08 11:32:22 -0400

    SAK-52643 SiteInfo avoid updating audit timestamps during conversion
    (cherry picked from commit 7121d5359828c242152d7aef9fb2ba57d6b4064e)

--- docs/conversion/sakai_25_mysql_conversion.sql
+++ docs/conversion/sakai_25_mysql_conversion.sql
@@ -4,6 +4,7 @@
 DELETE FROM SAKAI_MESSAGE_BUNDLE where PROP_VALUE is NULL;
 
 -- SAK-48106
+ALTER TABLE user_audits_log MODIFY audit_stamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
 UPDATE user_audits_log AS audits INNER JOIN SAKAI_USER_ID_MAP AS idmap ON audits.user_id = idmap.eid SET audits.user_id = idmap.user_id;
 UPDATE user_audits_log AS audits INNER JOIN SAKAI_USER_ID_MAP AS idmap ON audits.action_user_id = idmap.eid SET audits.action_user_id = idmap.user_id;
 -- END SAK-48106
